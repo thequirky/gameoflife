8417615013c92d7f7c3ea422e285305a4de78744
dingo <chatziva@protonmail.com>
2024-01-08 13:22:31 +0000

    remove debug stuff

--- creatures.py
+++ creatures.py
@@ -2,7 +2,6 @@ def to_coords(creature: list[str]) -> list[tuple[int, int]]:
     coords = []
     for row_nb, row_str in enumerate(creature):
         for col_nb, cell in enumerate(row_str):
-            print(cell, cell == "1", f" - ({row_nb},{col_nb})")
             if cell == "1":
                 coords.append((row_nb, col_nb))
     return coords
